e34bd8f6a2e6235ae2af5b9f173b3d10276b367f
claudiorhessel <claudiorhessel@gmail.com>
2023-04-24 13:50:12 -0300

    feat: create api controllers

---
+++ app/controllers/api/v1/LeaguesApi.class.php
@@ -0,0 +1,163 @@
+<?php
+namespace app\controllers\api\v1;
+
+use app\core\Controller;
+use app\models\LeaguesModel;
+use Helper;
+
+class LeaguesApi extends Controller {
+    public function index()
+    {
+        $params = [
+            "id" => isset($_GET["id"])? $_GET["id"] : null,
+            "referal_league_id" => isset($_GET["referal_league_id"])? $_GET["referal_league_id"] : null,
+            "name" => isset($_GET["name"])? $_GET["name"] : null,
+            "type" => isset($_GET["type"])? $_GET["type"] : null,
+            "country" => isset($_GET["country"])? $_GET["country"] : null,
+            "season" => isset($_GET["season"])? $_GET["season"] : null
+        ];
+
+        $leaguesModel = new LeaguesModel;
+        $data = $leaguesModel->getAllLeagues($params);
+		
+		http_response_code(200);
+		$return = [
+			"status" => "OK",
+			"data" => $data
+		];
+
+		print_r(json_encode($return));
+		die();
+    }
+
+    public function show($id)
+    {
+        $leaguesModel = new LeaguesModel;
+        $data = $leaguesModel->getLeagueById($id);
+		
+		http_response_code(200);
+		$return = [
+			"status" => "OK",
+			"data" => $data
+		];
+
+		print_r(json_encode($return));
+		die();
+    }
+
+    public function store()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+        $leaguesModel = new LeaguesModel;
+
+        $dataLeague = [
+            "referal_league_id" => $input["referal_league_id"],
+            "name" => $input["name"],
+            "type" => $input["type"],
+            "country" => $input["country"],
+            "country_code" => $input["country_code"],
+            "season" => $input["season"],
+            "season_start" => $input["season_start"],
+            "season_end" => $input["season_end"],
+            "logo" => $input["logo"],
+            "flag" => $input["flag"],
+            "standings" => $input["standings"],
+            "is_current" => $input["is_current"]
+        ];
+
+        $leagueId = $leaguesModel->createLeague($dataLeague);
+
+        $code = 201;
+        $return = [
+            "status" => "OK",
+            "message" => "Liga cadastrado com sucesso",
+            "data" => $leagueId
+        ];
+
+        if (!$leagueId) {
+            $code = 401;
+            $return = [
+                "status" => "Erro",
+                "message" => "Erro ao gravar liga"
+            ];
+        }
+
+        http_response_code($code);
+        print_r(json_encode($return));
+        die();
+    }
+
+    public function update($id)
+    {
+        $input = json_decode(file_get_contents('php://input'), TRUE);
+
+        $leaguesModel = new LeaguesModel;
+
+        $dataLeague = [
+            "referal_league_id" => $input["referal_league_id"],
+            "name" => $input["name"],
+            "type" => $input["type"],
+            "country" => $input["country"],
+            "country_code" => $input["country_code"],
+            "season" => $input["season"],
+            "season_start" => $input["season_start"],
+            "season_end" => $input["season_end"],
+            "logo" => $input["logo"],
+            "flag" => $input["flag"],
+            "standings" => $input["standings"],
+            "is_current" => $input["is_current"]
+        ];
+
+        $leagueUpdated = $leaguesModel->updateLeague($id, $dataLeague);
+
+        $code = 200;
+        $return = [
+            "status" => "OK",
+            "message" => "Liga atualizado com sucesso",
+            "data" => $leagueUpdated
+        ];
+
+        if (!$leagueUpdated) {
+            $code = 401;
+            $return = [
+                "status" => "Erro",
+                "message" => "Erro ao atualizar liga"
+            ];
+        }
+
+        http_response_code($code);
+        print_r(json_encode($return));
+        die();
+    }
+
+    public function destroy($id)
+    {
+        $leaguesModel = new LeaguesModel;
+
+        $dataLeague = [
+            "deletedAt" => date("Y-m-d H:i:s")
+        ];
+
+        $leagueUpdated = $leaguesModel->updateLeague($id, $dataLeague);
+
+        $code = 200;
+        $return = [
+            "status" => "OK",
+            "message" => "Liga deletada com sucesso",
+            "data" => $leagueUpdated
+        ];
+
+        if (!$leagueUpdated) {
+            $code = 401;
+            $return = [
+                "status" => "Erro",
+                "message" => "Erro ao deletar liga"
+            ];
+        }
+
+        http_response_code($code);
+        print_r(json_encode($return));
+        die();
+    }
+}
--- app/controllers/api/v1/TeamsApi.class.php
+++ app/controllers/api/v1/TeamsApi.class.php
@@ -1,29 +1,168 @@
 <?php
+namespace app\controllers\api\v1;
 
-$curl = curl_init();
+use app\core\Controller;
+use app\models\TeamsModel;
+use Helper;
 
-curl_setopt_array($curl, [
-	CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v2/leagues/type/league/england/2019",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"X-RapidAPI-Host: api-football-v1.p.rapidapi.com",
-		"X-RapidAPI-Key: 4e081b6d5dmsh213846c08567d5fp1151c8jsn53cc4b1d7139"
-	],
-]);
+class TeamsApi extends Controller {
+    public function index()
+    {
+        $params = [
+            "id" => isset($_GET["id"])? $_GET["id"] : null,
+            "name" => isset($_GET["name"])? $_GET["name"] : null,
+            "code" => isset($_GET["code"])? $_GET["code"] : null,
+            "country" => isset($_GET["country"])? $_GET["country"] : null,
+            "referal_league_id" => isset($_GET["referal_league_id"])? $_GET["referal_league_id"] : null,
+            "league_id" => isset($_GET["league_id"])? $_GET["league_id"] : null,
+            "league" => isset($_GET["league"])? $_GET["league"] : null
+        ];
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+        $teamsModel = new TeamsModel;
+        $data = $teamsModel->getAllTeams($params);
+        
+        http_response_code(200);
+        $return = [
+            "status" => "OK",
+            "data" => $data
+        ];
 
-curl_close($curl);
+        print_r(json_encode($return));
+        die();
+    }
 
-if ($err) {
-	echo "cURL Error #:" . $err;
-} else {
-	echo $response;
+    public function show($id)
+    {
+        $teamsModel = new TeamsModel;
+        $data = $teamsModel->getTeamById($id);
+        
+        http_response_code(200);
+        $return = [
+            "status" => "OK",
+            "data" => $data
+        ];
+
+        print_r(json_encode($return));
+        die();
+    }
+
+    public function store()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+        $teamsModel = new TeamsModel;
+
+        $dataTeam = [
+            "league_id" => $input["league_id"],
+            "referal_league_id" => $input["referal_league_id"],
+            "referal_team_id" => $input["referal_team_id"],
+            "name" => $input["name"],
+            "code" => $input["code"],
+            "logo" => $input["logo"],
+            "country" => $input["country"],
+            "is_national" => $input["is_national"],
+            "founded" => $input["founded"],
+            "venue_name" => $input["venue_name"],
+            "venue_surface" => $input["venue_surface"],
+            "venue_address" => $input["venue_address"],
+            "venue_city" => $input["venue_city"],
+            "venue_capacity" => $input["venue_capacity"]
+        ];
+
+        $teamId = $teamsModel->createTeam($dataTeam);
+
+        $code = 201;
+        $return = [
+            "status" => "OK",
+            "message" => "Time cadastrado com sucesso",
+            "data" => $teamId
+        ];
+
+        if (!$teamId) {
+            $code = 401;
+            $return = [
+                "status" => "Erro",
+                "message" => "Erro ao gravar time"
+            ];
+        }
+
+        http_response_code($code);
+        print_r(json_encode($return));
+        die();
+    }
+
+    public function update($id)
+    {
+        $input = json_decode(file_get_contents('php://input'), TRUE);
+
+        $teamsModel = new TeamsModel;
+
+        $dataTeam = [
+            "league_id" => $input["league_id"],
+            "referal_league_id" => $input["referal_league_id"],
+            "referal_team_id" => $input["referal_team_id"],
+            "name" => $input["name"],
+            "code" => $input["code"],
+            "logo" => $input["logo"],
+            "country" => $input["country"],
+            "is_national" => $input["is_national"],
+            "founded" => $input["founded"],
+            "venue_name" => $input["venue_name"],
+            "venue_surface" => $input["venue_surface"],
+            "venue_address" => $input["venue_address"],
+            "venue_city" => $input["venue_city"],
+            "venue_capacity" => $input["venue_capacity"]
+        ];
+
+        $teamUpdated = $teamsModel->updateTeam($id, $dataTeam);
+
+        $code = 200;
+        $return = [
+            "status" => "OK",
+            "message" => "Time atualizado com sucesso",
+            "data" => $teamUpdated
+        ];
+
+        if (!$teamUpdated) {
+            $code = 401;
+            $return = [
+                "status" => "Erro",
+                "message" => "Erro ao atualizar time"
+            ];
+        }
+
+        http_response_code($code);
+        print_r(json_encode($return));
+        die();
+    }
+
+    public function destroy($id)
+    {
+        $teamsModel = new TeamsModel;
+
+        $dataTeam = [
+            "deletedAt" => date("Y-m-d H:i:s")
+        ];
+
+        $teamUpdated = $teamsModel->updateTeam($id, $dataTeam);
+
+        $code = 200;
+        $return = [
+            "status" => "OK",
+            "message" => "Time deletado com sucesso",
+            "data" => $teamUpdated
+        ];
+
+        if (!$teamUpdated) {
+            $code = 401;
+            $return = [
+                "status" => "Erro",
+                "message" => "Erro ao deletar time"
+            ];
+        }
+
+        http_response_code($code);
+        print_r(json_encode($return));
+        die();
+    }
 }
